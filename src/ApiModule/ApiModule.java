e76a82340947b53829e1203454ff3211978c8c10
ABOUHALI <abouhaliayman3@gmail.com>
2023-03-08 15:39:24 +0100

    first push

--- src/ApiModule/ApiModule.java
+++ src/ApiModule/ApiModule.java
@@ -1,31 +1,143 @@
 package ApiModule;
 
 import DataStore.JSONDataStore;
-import DataStore.UserInfo;
+import DataStore.UserData;
 import Configuration.Configuration;
+import Configuration.ServiceInformation;
 
-interface SessionManager {
-    void newSession(int id, int service, int time);
-    InvoiceInformation[] invoice();
-    UserInfo[] getUserList();
-    double calculateBandwidth(int service, double signalStrength);
-}
-
-class InvoiceInformation {
-    public String name;
-    public double dataVolume;
-    public int voiceMinutes;
-}
+import java.lang.Math.min;
+import java.lang.RuntimeException;
+import java.util.Random;
 
 class ApiModule implements SessionManager {
 
     private Configuration config;
-    private DataStore data;
+    private DataStore dataStore;
+    private List<UserData> users;
 
-    public ApiModule(){
-        this.config = new Configuration();
-        this.data = new JSONDataStore();
+    public ApiModule(Configuration config, DataStore dataStore){
+        this.config = config;
+        this.dataStore = dataStore;
+        this.users = dataStore.loadUserData();
+    }
 
+    public void newSession(int userIndex, String serviceId, int time){
+        // TODO: check input!!!
+        
+        switch (this.config.getSessionType(service)) {
+            case config.SessionType.VOICE_SESSION:
+                this.voiceSession(userIndex, time);
+                break;
+            case config.SessionType.DATA_SESSION:
+                this.dataSession(userIndex, serviceId, time);
+                break;
+        }
+    }
+
+    private void voiceSession(int userIndex, int time) {
+        UserData user = this.users.get(userIndex);
+
+        int freeMinutes = user.getFreeMinutes();
+        int charge = 0;
+        if (time>freeMinutes){
+            int chargedMinutes = time-freeMinutes;
+            String subscriptionType = user.getSubscriptionType();
+            charge = chargedMinutes * this.config.getPricePerMinute(subscriptionType);
+            freeMinutes = 0;
+        } else { //enough free minutes
+            freeMinutes -= time;
+        }
+        user.setFreeMinutes(freeMinutes);
+        user.setVoiceCharge(user.getVoiceCharge() + charge);
+
+        // store data?
+    }   
+
+    private void dataSession(String userIndex, String serviceId, int time) {
+        UserData user = this.users.get(userIndex);
+        
+        String subscriptionType = user.getSubscriptionType();
+        double subscriptionRate = this.config.getSubscriptionDataRate(subscriptionType);
+        double signalStrength = this.getSignalStrength();
+        double availableRate = subscriptionRate*signalStrength;
+
+        double requiredRate = this.config.getRequiredDataRate(serviceId);
+
+        double dataRate = min(requiredRate, availableRate);
+
+        double dataCost = dataRate * time;
+        double dataVolume = user.getDataVolume();
+
+        if (dataCost < dataVolume) {
+            user.setRemainingData(dataVolume-dataCost);
+        } else {
+            throw new NotEnoughDataVolumeException();
+        }
+    }
+
+    private double getSignalStrength(){
+        int random = new Random().nextInt(3);
+        switch(random){
+            case 0: // N/A
+                return 0.0;
+            case 1: // bad
+                return 0.1;
+            case 2: // middle
+                return 0.25;
+            case 3: // good
+                return 0.5;
+        }
+    }
+
+    public List<InvoiceInformation> invoice() {
+        List<InvoiceInformation> invoices = new List<InvoiceInformation>();
+
+        for (UserData user : this.users) {
+            invoices.add(this.invoiceUser(user));
+        }
+        return invoices;
+    }
+
+    private InvoiceInformation invoiceUser(UserData user) {
+
+        // get invoice data
+        String subscriptionType = user.getSubscriptionType();
+        int basePrice = this.config.getBasePrice(user.getSubscriptionType());
+        InvoiceInformation invoice = new InvoiceInformation(user, basePrice);
+
+        // reset user data
+        int freeMinutes = this.config.getSubscriptionFreeMinutes(subscriptionType);
+        int dataVolume = this.config.getSubscriptionDataVolume(subscriptionType);
+        user.setFreeMinutes(freeMinutes);
+        user.setRemainingData(dataVolume);
+        user.setVoiceCharge(0);
+
+        return invoice;
+    }
+
+    public List<UserData>getUserList() {
+        return this.users;
+    }
+
+    public void addUser(UserData newUser){
+        //check if user is duplicate
+        String imsi = newUser.getImsi();
+        for (UserData user :  this.users){
+            if (imsi.equals(user.getImsi())){
+                throw new UserAlreadyExistsException("A User with that IMSI already exists!");
+            }
+        }
+
+        this.users.add(user);
     }
 }
+
+class NotEnoughDataVolumeException extends RuntimeException {
+    super("Not enough Data Volume!");
+}
+
+class UserAlreadyExistsException extends RuntimeException {
+    super("Not enough Data Volume!");
+}
+
 
--- src/ApiModule/InvoiceInformation.java
+++ src/ApiModule/InvoiceInformation.java
@@ -10,11 +10,11 @@ public class InvoiceInformation {
     public int voiceCharge;
     public int basePrice;
 
-    public InvoiceInformation(UserData userData, int voiceCharge, int basePrice){
+    public InvoiceInformation(UserData userData, int basePrice){
         this.name = userData.getName(); //TODO: check what is to be displayed
         this.dataVolume = userData.getDataVolume();
         this.voiceMinutes = userData.getDataVolume();
-        this.voiceCharge = voiceCharge;
+        this.voiceCharge = userData.getVoiceCharge();
         this.basePrice = basePrice;
     }
 }
