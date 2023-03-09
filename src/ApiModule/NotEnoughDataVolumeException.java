de56894923f9b86eb0d391ad074420aaae64be76
Andreas Gräwingholt <andreas.graewingholt@decadia.com>
2023-03-09 11:55:01 +0100

    doc(API): exceptions

--- src/ApiModule/NotEnoughDataVolumeException.java
+++ src/ApiModule/NotEnoughDataVolumeException.java
@@ -1,5 +1,12 @@
 package ApiModule;
 
+/**
+ * Exception thrown when a session is rejected because the subscriber does not have enough data volume left
+ *  
+ * @author ALARA
+ *
+ * @since 1.0
+ */
 public class NotEnoughDataVolumeException extends RuntimeException {
 	public NotEnoughDataVolumeException() {
 		super("Not enough Data Volume!");
--- src/ApiModule/UserIndexOutOfBoundsException.java
+++ src/ApiModule/UserIndexOutOfBoundsException.java
@@ -1,5 +1,12 @@
 package ApiModule;
 
+/**
+ * Exception thrown when trying to get a user from the list of all users with an invalid index
+ *  
+ * @author ALARA
+ *
+ * @since 1.0
+ */
 class UserIndexOutOfBoundsException extends RuntimeException {
 	public UserIndexOutOfBoundsException() {
 		super("User index is out of bounds!");
