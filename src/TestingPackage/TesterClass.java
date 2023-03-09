e5d3cf732e0219d7c6d4de5c799fd2da185ac7f2
Asmae Mahjoubi <asmae.mahjoubi@alumni.fh-aachen.de>
2023-03-09 10:42:36 +0100

    test class

---
+++ src/TestingPackage/TesterClass.java
@@ -0,0 +1,64 @@
+package TestingPackage;
+
+
+
+
+import java.io.IOException;
+import java.util.List;
+
+import ApiModule.ApiModule;
+import ApiModule.InvoiceInformation;
+import Configuration.ConfigurationImp;
+import DataStore.JsonDataStore;
+import DataStore.UserData;
+
+public class TesterClass {
+	public static void main(String[] args) throws IOException {
+		
+	
+	ConfigurationImp config = new ConfigurationImp();
+    JsonDataStore dataStore = new JsonDataStore();
+    
+    
+    ApiModule api = new ApiModule(config, dataStore);
+    
+    List<UserData> userList = api.getUserList();
+    System.out.println("Initial User List: " + userList);
+    
+    
+    
+    UserData newUser = new UserData("Asmae", "Mahjoubi", "124565645","Terminal type", "Subscription A");
+    api.addUser(newUser);
+    System.out.println("we've added the user: " + newUser);
+    System.out.println("The user List is : " + api.getUserList());
+    
+    api.removeUser(0);
+    System.out.println("i've removed user: " + api.getUserList());
+    
+    
+    api.addUser(newUser);
+    System.out.println("User added: " + newUser);
+    api.newSession(0, "DATA", 10);
+    System.out.println("Data session created for user: " + newUser);
+    
+    
+    api.newSession(0, "VOICE", 30);
+    System.out.println("Voice session created for user: " + newUser);
+    
+    
+    List<InvoiceInformation> invoices = api.invoice();
+    System.out.println("Invoices generated: " + invoices);
+    
+    // Test saveData method
+    api.saveData();
+    System.out.println("Data saved.");
+    
+    
+    
+    
+    
+    
+  
+	}
+
+}
