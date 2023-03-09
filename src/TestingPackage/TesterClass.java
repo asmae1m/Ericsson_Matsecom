067a4f6adbf250103a80d71a0349906b49c36003
Asmae Mahjoubi <asmae.mahjoubi@alumni.fh-aachen.de>
2023-03-09 14:49:38 +0100

    Methods test Works

--- src/TestingPackage/TesterClass.java
+++ src/TestingPackage/TesterClass.java
@@ -1,19 +1,19 @@
 package TestingPackage;
 
 
-
-
 import java.io.IOException;
 import java.util.List;
 
 import ApiModule.ApiModule;
 import ApiModule.InvoiceInformation;
 import Configuration.ConfigurationImp;
+import Configuration.Ran;
+import Configuration.SessionType;
 import DataStore.JsonDataStore;
 import DataStore.UserData;
 
 public class TesterClass {
-	public static void main(String[] args) throws IOException {
+	public static void main(String args[]) throws IOException {
 		
 	
 	ConfigurationImp config = new ConfigurationImp();
@@ -24,26 +24,25 @@ public class TesterClass {
     
     List<UserData> userList = api.getUserList();
     System.out.println("Initial User List: " + userList);
+    System.out.println("the string value is : "+Ran.twoG.getStringValue());
     
     
-    
-    UserData newUser = new UserData("Asmae", "Mahjoubi", "124565645","Terminal type", "Subscription A");
+    UserData newUser = new UserData("Asmae", "Mahjoubi", "114565645",Ran.twoG.getStringValue(), "Subscription A");
     api.addUser(newUser);
-    System.out.println("we've added the user: " + newUser);
+    System.out.println("we've added the user: " + newUser.getImsi());
     System.out.println("The user List is : " + api.getUserList());
     
-    api.removeUser(0);
-    System.out.println("i've removed user: " + api.getUserList());
+    /*api.removeUser(1);
+    System.out.println("i've removed user: " + api.getUserList());*/
     
+    UserData newUser1 = new UserData("Person", "Good", "114465645",Ran.twoG.getStringValue(), "Subscription B");
+    api.addUser(newUser1);
     
-    api.addUser(newUser);
-    System.out.println("User added: " + newUser);
-    api.newSession(0, "DATA", 10);
+    System.out.println("User added: " + newUser1);
+    
+    api.newSession(1, "Voice call", 20);
     System.out.println("Data session created for user: " + newUser);
     
-    
-    api.newSession(0, "VOICE", 30);
-    System.out.println("Voice session created for user: " + newUser);
     
     
     List<InvoiceInformation> invoices = api.invoice();
