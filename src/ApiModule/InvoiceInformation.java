de31746d846f1d8395ab6e397f360f0a92fb4d26
Andreas Gräwingholt <andreas.graewingholt@decadia.com>
2023-03-09 11:32:37 +0100

    feat(API): add documentation for invoice info

--- src/ApiModule/InvoiceInformation.java
+++ src/ApiModule/InvoiceInformation.java
@@ -1,5 +1,12 @@
 package ApiModule;
 
+/**
+ * Data-object containing all relevant information for displaying an invoice for one subscriber
+ * 
+ * @author ALARA
+ * 
+ * @since 1.0
+ */
 public class InvoiceInformation {
     public String forename;
     public String surname;
@@ -8,6 +15,16 @@ public class InvoiceInformation {
     public int voiceCharge;
     public int basePrice;
 
+    /**
+     * Main constructor
+     * 
+     * @param forename: First name of the subscriber
+     * @param surname: Family name of the subscriber
+     * @param dataVolume: Total amount of data used since last invoice in MB
+     * @param voiceMinutes: Total amount of voice minutes since last invoice in minutes 
+     * @param voiceCharge: Total charges based on voice minutes, taking free minutes into account in Euro-cent
+     * @param basePrice: Base price of the users subscription in Euro-cent
+     */
     public InvoiceInformation(String forename, String surname, double dataVolume, int voiceMinutes, int voiceCharge, int basePrice){
         this.forename = forename;
         this.surname = surname;
