b816b383b564b60e92dc5b69bd9a4962307b31c8
ABOUHALI <abouhaliayman3@gmail.com>
2023-03-09 11:14:10 +0100

    add main config class

---
+++ src/Configuration/test.java
@@ -0,0 +1,11 @@
+package Configuration;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ConfigurationImp con = new ConfigurationImp();
+		con.saveProperty("key", "prop", "cmt");
+	}
+
+}
